694d0c29040418ede2d43b0cf28ef6d50b797320
catperez81 <cat@healthsherpa.com>
2015-12-08 16:38:32 -0800

    initial files

---
+++ js/application.js
@@ -0,0 +1,17 @@
+
+// $(document).ready(function() {
+//   alert('Oh, hi there, world');
+// });
+
+
+$(document).ready(function() {
+
+  $('.city-item').click(function() {
+    // console.log('test');
+  	$('.container').hide();
+  	$('.container-city').show();
+  })
+
+});  
+
+  
