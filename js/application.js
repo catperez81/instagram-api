14a3efe9990db84af35e720e6271ea148ec99916
catperez81 <cat@healthsherpa.com>
2015-12-09 18:08:36 -0800

    test token presence

--- js/application.js
+++ js/application.js
@@ -6,6 +6,9 @@
 
 $(document).ready(function() {
 
+	var accessToken = getUrlParameter ("access_token");
+	alert(accessToken);
+
   $('.city-item').click(function() {
     // console.log('test');
   	$('.container').hide();
@@ -28,3 +31,18 @@ function getRequest(searchTag){
     console.log(data.items);
   });
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
